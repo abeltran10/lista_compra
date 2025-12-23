cb50c7a76fa2a47193b0f908828d26fa8bd5a14a
abeltran10 <you@example.com>
2025-12-23 16:08:54 +0100

    Edición de producto-view.fxml

--- src/main/java/com/abeltran10/lista_compra/MainApplication.java
+++ src/main/java/com/abeltran10/lista_compra/MainApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/producto-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Lista de la compra");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/abeltran10/lista_compra/controllers/ProductoController.java
@@ -0,0 +1,21 @@
+package com.abeltran10.lista_compra.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ProductoController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    public void onGuardar() {}
+
+    @FXML
+    public void onCrear() {}
+
+    @FXML
+    public void onEditar() {}
+
+    @FXML
+    public void onEliminar() {}
+}
