2b3691590f4e35d401fcdff4a1f5e99342ee76f3
abeltran10 <you@example.com>
2025-12-28 22:43:58 +0100

    Método findAlimentacion en ProductoDAO

--- src/main/java/com/abeltran10/lista_compra/dao/ProductoDAO.java
+++ src/main/java/com/abeltran10/lista_compra/dao/ProductoDAO.java
@@ -1,6 +1,22 @@
 package com.abeltran10.lista_compra.dao;
 
+import com.abeltran10.lista_compra.model.Producto;
+import com.abeltran10.lista_compra.utils.JPA;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
 public class ProductoDAO {
+    private EntityManager em = JPA.getEntityManager();
 
-
+    public List<Producto> findAlimentacion() {
+        try {
+            return em.createQuery(
+                    "SELECT p FROM Producto p WHERE TYPE(p) = Alimentacion",
+                    Producto.class
+            ).getResultList();
+        } finally {
+            em.close();
+        }
+    }
 }
