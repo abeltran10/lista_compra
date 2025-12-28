53c8fcd49352d121c2e8e1fbfa95d68125b72867
abeltran10 <you@example.com>
2025-12-28 22:44:41 +0100

    Método obtenerTodos en ProductoService

--- src/main/java/com/abeltran10/lista_compra/service/ProductoService.java
+++ src/main/java/com/abeltran10/lista_compra/service/ProductoService.java
@@ -1,4 +1,16 @@
 package com.abeltran10.lista_compra.service;
 
+import com.abeltran10.lista_compra.dao.ProductoDAO;
+import com.abeltran10.lista_compra.model.Alimentacion;
+import com.abeltran10.lista_compra.model.Producto;
+
+import java.util.List;
+
 public class ProductoService {
+
+    private final ProductoDAO dao = new ProductoDAO();
+
+    public List<Producto> obtenerTodos() {
+        return dao.findAlimentacion();
+    }
 }
