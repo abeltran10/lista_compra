60b2657062126f2d15cae166bc5e88b693781a4d
abeltran10 <you@example.com>
2026-01-08 16:26:40 +0100

    Ejecutable

---
+++ src/main/resources/build/lista_compra.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ruta de los JARs
+JAVA_FX=/usr/share/openjfx/lib
+# Ruta típica de las librerías nativas en Debian 11/12
+NATIVE_LIBS=/usr/lib/x86_64-linux-gnu/jni
+
+java \
+-Djava.library.path="$NATIVE_LIBS" \
+--module-path "$JAVA_FX" \
+--add-modules javafx.controls,javafx.fxml,javafx.graphics \
+-jar "$HOME/lista_compra-v1.0.0.jar"
+
+
+
